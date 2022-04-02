6eacaf20ba6705e7d5d139e6823566ce1ce01421
najib <narujib13@gmail.com>
2022-04-02 15:13:24 +0700

    upDB

--- db_ekom(4).sql
+++ db_ekom(4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 29, 2022 at 01:25 AM
+-- Generation Time: Apr 02, 2022 at 10:11 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -37,22 +37,10 @@ CREATE TABLE `jurusan` (
 --
 
 INSERT INTO `jurusan` (`id`, `jurusan`) VALUES
-(4, 'Bisnis Konstruksi dan Properti');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `post`
---
-
-CREATE TABLE `post` (
-  `id` int(11) NOT NULL,
-  `title` varchar(128) NOT NULL,
-  `image` varchar(128) NOT NULL,
-  `des` varchar(256) NOT NULL,
-  `price` varchar(128) NOT NULL,
-  `date_created` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+(4, 'Admin'),
+(20, 'Bisnis Konstruksi dan Properti'),
+(21, 'Teknik Elektronika Industri'),
+(22, 'Teknik Instalasi Tenaga Listrik');
 
 -- --------------------------------------------------------
 
@@ -77,13 +65,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `jurusan_id`, `date_created`) VALUES
-(1, 'Najib', 'najib@gmail.com', 'default1.jpg', '$2y$10$I88gRGIEAgPh3wnpHHRiueiwBcc39vY12KU68pEJ72TSAHwiVlvfO', 1, 1, 0, 1647899825),
-(2, 'Taro', 'taro@gmail.com', 'default.jpg', '$2y$10$8FTrP19sqbAbaLYIg.CVDOJEeVpb0BthefqYSNnw0O2uGOPts4F.W', 2, 1, 0, 1647900209),
-(3, 'Conan', 'conan@gmail.com', 'default.jpg', '$2y$10$Dt4MOk8itVvTCAHQixSVk.DmhgKFRGNtEUVh2qpQsKR2dlOYo6a76', 2, 0, 0, 1647900339),
-(4, 'Hamtaro', 'hamtaro@gmail.com', 'default.jpg', '$2y$10$iDrpCQkA.VpFjnKFeLBpdO/T3yqWKyiIulxMnHw0LGfgGafP5uLQK', 2, 1, 0, 1647900427),
-(5, 'Gon', 'gon@gmail.com', 'default.jpg', '$2y$10$NLVUe2.uIoiRCdZ98e1db.j3nFlUdStsCUaHzAZMmoig8EOG8md2K', 2, 1, 0, 1647900490),
-(6, 'Patrik', 'patrik@gmail.com', 'default.jpg', '$2y$10$wxJ5fHwH72l03TvkIHZ8Aud33Pu4fEs5NVrltUlYioAVs18nEo3y6', 2, 1, 1, 1648491542),
-(7, 'Oscar', 'oscar@gmail.com', 'default.jpg', '$2y$10$xsgDaPLie9HSaYP7QWkEZeNf2egX7fIBMEVrAr8NIjN1TBQFOe83m', 2, 1, 2, 1648493196);
+(1, 'Najib', 'najib@gmail.com', 'default1.jpg', '$2y$10$I88gRGIEAgPh3wnpHHRiueiwBcc39vY12KU68pEJ72TSAHwiVlvfO', 1, 1, 4, 1647899825),
+(14, 'Conan', 'conan@gmail.com', 'default.jpg', '$2y$10$AU52vuqWJg2eWzy6LmsVn.jiNJZ6T4dqqHUWCeRZAvRfUMy73tMBe', 2, 1, 20, 1648854635),
+(15, 'Taro', 'taro@gmail.com', 'default.jpg', '$2y$10$qR55WD384hDXHnDN8cFntOlGR3/GkApYwsyBbbKiOc/MaQeeFDFqe', 1, 1, 4, 1648854687),
+(16, 'erer', 'oscar@gmail.com', 'default.jpg', '$2y$10$bA9n9FWkZlI6PAwnjhiM4e3abLKVgaXvGPcnLt93VnNkt5WLsreWe', 1, 1, 4, 1648854856);
 
 -- --------------------------------------------------------
 
@@ -105,9 +90,7 @@ INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
 (1, 1, 1),
 (2, 1, 2),
 (3, 2, 2),
-(4, 1, 3),
-(5, 1, 6),
-(6, 1, 7);
+(4, 1, 3);
 
 -- --------------------------------------------------------
 
@@ -127,9 +110,7 @@ CREATE TABLE `user_menu` (
 INSERT INTO `user_menu` (`id`, `menu`) VALUES
 (1, 'Administrator'),
 (2, 'User'),
-(3, 'Menu'),
-(6, 'coba'),
-(7, 'oke');
+(3, 'Menu');
 
 -- --------------------------------------------------------
 
@@ -148,8 +129,7 @@ CREATE TABLE `user_role` (
 
 INSERT INTO `user_role` (`id`, `role`) VALUES
 (1, 'Administrator'),
-(2, 'Member'),
-(3, 'new');
+(2, 'Member');
 
 -- --------------------------------------------------------
 
@@ -176,10 +156,11 @@ INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active
 (3, 2, 'Edit Profile', 'user/editprofile', 'fas fa-fw fa-user-edit', 1),
 (4, 3, 'Menu Management', 'menu', 'fas fa-fw fa-folder', 1),
 (5, 3, 'Sub Menu Management', 'menu/submenu', 'fas fa-fw fa-folder-open', 1),
-(6, 1, 'Role', 'administrator/role', 'fas fa-fw fa-user-tie', 1),
-(16, 2, 'Change Password', 'user/changepassword', 'fas fa-fw fa-horse', 1),
-(17, 1, 'Create Account', 'administrator/createaccount', 'fas fa-fw fa-horse', 1),
-(18, 1, 'Jurusan', 'administrator/jurusan', 'fas fa-fw fa-horse', 1);
+(6, 1, 'Role', 'administrator/role', 'fas fa-fw fa-user-cog', 1),
+(16, 2, 'Change Password', 'user/changepassword', 'fas fa-fw fa-key', 1),
+(23, 1, 'Accounts', 'administrator/accounts', 'fas fa-fw fa-users', 1),
+(24, 1, 'Jurusan', 'administrator/jurusan', 'fas fa-fw fa-tasks', 1),
+(25, 2, 'Product', 'user/product', 'fas fa-fw fa-horse', 1);
 
 --
 -- Indexes for dumped tables
@@ -189,12 +170,6 @@ INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active
 -- Indexes for table `jurusan`
 --
 ALTER TABLE `jurusan`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `post`
---
-ALTER TABLE `post`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -235,19 +210,13 @@ ALTER TABLE `user_sub_menu`
 -- AUTO_INCREMENT for table `jurusan`
 --
 ALTER TABLE `jurusan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `post`
---
-ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `user_access_menu`
@@ -259,19 +228,19 @@ ALTER TABLE `user_access_menu`
 -- AUTO_INCREMENT for table `user_menu`
 --
 ALTER TABLE `user_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user_role`
 --
 ALTER TABLE `user_role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `user_sub_menu`
 --
 ALTER TABLE `user_sub_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
